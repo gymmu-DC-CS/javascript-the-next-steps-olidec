aff7bb1ad38ac8f97f676fe416172a12282d5ef7
olidec <69003898+olidec@users.noreply.github.com>
2023-11-28 15:35:53 +0100

    exercises 2 + 3

--- script.js
+++ script.js
@@ -27,3 +27,21 @@ export function exercise01(args) {
 
   return result.join("")
 }
+
+export function exercise02(args) {
+  return args.toUpperCase()
+}
+
+export function exercise03(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E") {
+      result++
+    }
+  }
+
+  return result
+}
